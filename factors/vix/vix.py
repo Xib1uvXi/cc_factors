fd119637de19bf2895442789c454976ae0febb3d
wangxi <xib1102@icloud.com>
2022-08-24 16:40:53 +0800

    add vic

--- factors/vix/vix.py
+++ factors/vix/vix.py
@@ -9,8 +9,40 @@ import pandas_ta as ta
 
 @dataclass
 class State:
-    corr: float
     df: pd.DataFrame
+
+    def cu_up(self, priod: int = 1) -> Optional[pd.DataFrame]:
+        df = self.df.copy()
+
+        df["diff_cu_up"] = df["vix"] - df["vix_ma_up"]
+        df["signal_cu_up"] = (df["diff_cu_up"] < 0) & (df["diff_cu_up"].shift() >= 0)
+
+        df["cu_up_priod_success"] = df["signal_cu_up"] & (df["close"].shift(-1 * priod) - df["close"] < 0)
+        df["priod_diff_close"] = df["close"] - df["close"].shift(-1 * priod)
+
+        # Indicators
+
+        # win rate
+        win_rate = df["cu_up_priod_success"].sum() / df["signal_cu_up"].sum()
+
+        # net_values
+        signal_close = df[df["signal_cu_up"]]["close"]
+        signal_priod_diff_close = df[df["signal_cu_up"]]["priod_diff_close"]
+        amplitudes = signal_priod_diff_close / signal_close
+        net_values = (1 + amplitudes).cumprod()
+
+        # calculate correlation
+        corr = (
+            df[["signal_cu_up", "cu_up_priod_success"]]
+            .corr(method="spearman")
+            .loc["signal_cu_up", "cu_up_priod_success"]
+        )
+
+        print("correlation", corr)
+        print("win_rate", win_rate)
+        print("net_values", net_values[-1])
+
+        return df
 
 
 @dataclass
@@ -21,27 +53,22 @@ class Model:
     state: Optional[State] = None
 
     def fit(self, close: pd.DataFrame) -> Self:
+        # build indicator
         df = self.indicator(close)
-
-        # calculate correlation
-        corr = df[["close", "vix_ma"]].corr(method="spearman").loc["close", "vix_ma"]
-
-        self.state = State(corr, df)
+        self.state = State(df)
 
         return self
 
     def indicator(self, df: pd.DataFrame) -> pd.DataFrame:
-        vix = np.log(df["close"]) / np.log(df["close"].shift(self.length - 1)) - 1
+        vix = np.log(df["close"]) / np.log(df["close"].shift(self.length)) - 1
         vix_ma: pd.Series = ta.sma(vix, 20)
+
+        assert vix_ma is not None
 
         # build indicator
         df["vix"] = vix
         df["vix_ma"] = vix_ma
         df["vix_ma_up"] = vix_ma.rolling(self.length, min_periods=0).max()
         df["vix_ma_down"] = vix_ma.rolling(self.length, min_periods=0).min()
-
-        # FIXME: this is not working
-        df["diff"] = df["vix"] - df["vix_ma_up"]
-        df["signal"] = (df["diff"] < 0) & (df["diff"].shift() >= 0)
 
         return df
--- tests/vix/test_vix.py
+++ tests/vix/test_vix.py
@@ -6,20 +6,9 @@ from factors.vix.vix import Model
 def test_vix():
     symbol = "filusdt"
     interval = "1d"
-    start_time = datetime(2021, 1, 1, 0, 0, 0)
-    end_time = datetime(2021, 12, 1, 23, 59, 0)
+    start_time = datetime(2020, 10, 16, 1, 1, 0)
+    end_time = datetime(2022, 8, 16, 7, 59, 0)
     close = get_close_prices(symbol, interval, start_time, end_time)
 
-    vix_model = Model(length=36).fit(close)
-
-    # print(vix_model.state.corr)
-    # print(vix_model.state.df)
-
-    df = vix_model.state.df
-
-    priod = 1
-    df["success"] = (df["close"] < df["close"].shift(priod)) & df["signal"].shift(priod) == 1
-
-    print(df["success"].sum() / df["signal"].sum() * 100)
-
-    print(df[df["signal"]])
+    vix_model = Model(length=5).fit(close)
+    vix_model.state.cu_up(2)
