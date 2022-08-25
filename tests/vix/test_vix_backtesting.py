9817fe4783db20de5935a44317ba6fb921211222
wangxi <xib1102@icloud.com>
2022-08-25 11:01:06 +0800

    add backtesting

--- tests/vix/test_vix_backtesting.py
+++ tests/vix/test_vix_backtesting.py
@@ -31,3 +31,18 @@ def test_vix_backtesting_1():
     # print("raw capital: ", bt.raw_capital, "capital: ", bt.capital)
     # print("raw pos: ", bt.raw_pos, "pos: ", bt.pos)
     print("2021/1/1 - 2022/8/16  fil return", f"{100*((bt.pos - bt.raw_pos) / bt.raw_pos):,.2f}%")
+
+
+def test_vix_backtesting_2():
+    symbol = "filusdt"
+    interval = "1h"
+    start_time = datetime(2021, 10, 1, 0, 0, 0)
+    end_time = datetime(2022, 8, 16, 0, 0, 0)
+    close = get_close_prices(symbol, interval, start_time, end_time)
+
+    bt = VixBacktesting(close)
+    bt.run()
+
+    # print("raw capital: ", bt.raw_capital, "capital: ", bt.capital)
+    # print("raw pos: ", bt.raw_pos, "pos: ", bt.pos)
+    print("2021/10/1 - 2022/8/16  fil return", f"{100*((bt.pos - bt.raw_pos) / bt.raw_pos):,.2f}%")
